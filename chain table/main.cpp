2e406d103e110be48d25ff808e994a2269b7c003
1004385346 <1004385346@qq.com>
2015-12-09 18:41:33 +0800

    链表
    在使用链表之前先要创建一个结构体。在结构体中设置结构体的值域和指针域。在使用链表这种方法的时候，先创建3个结构体指针，利用第一个指针创建头节点。
    使p指向头节点，然后用循环时q指向新的结构题，p的下一个指向q来创建链表，循环结束时p归0；最后在使用完链表后用free将节点释放。

--- chain table/main.cpp
+++ chain table/main.cpp
@@ -7,9 +7,41 @@
 //
 
 #include <iostream>
-
+ struct student
+    {
+        char name[50];
+        int num;
+        struct student *next;
+        
+    };
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+
+    int i;
+    student *p,*q,*head;
+    head=new student;
+    head->next=0;
+    p=head;
+    for(i=0;i<4;i++)
+    {
+        q=new student;
+        p->next=q;
+        q->next=0;
+        scanf("%s %d",q->name,&(q->num));
+        p=q;
+    }
+    p=head->next;
+    while (p!=0)
+    {
+        printf("%s %d\n",p->name,p->num);
+        p=p->next;
+    }
+    while(p!=0)
+    {
+        q=p;
+        p=p->next;
+        free(q);
+        
+    }
     return 0;
 }
